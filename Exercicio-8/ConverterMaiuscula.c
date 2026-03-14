8991065fe486df10ad6d00319c1ab66499ea3022
Iarley Souza <iarleysouza615@gmail.com>
2026-03-14 16:44:53 -0300

    up

--- Exercicio-8/ConverterMaiuscula.c
+++ Exercicio-8/ConverterMaiuscula.c
@@ -1,16 +1,40 @@
-#include <string.h>
+/**
+ * @brief Converte todos os caracteres da string para maiúsculas
+ * @param str String a ser modificada (deve ser modificável)
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+char maiuscula(char c) { // função para poder converter minuscula em maiuscula.
+    if (c >= 'a' && c <= 'z') {
+        return c - 32;
+    }
+    return c;
+}
 
 void converter_para_maiusculas(char *str) {
-    while (*str) {
-        *str = toupper(*str);
+    while (*str) { //Continue enquanto o caractere atual não for o final da string ('\0').
+        *str = maiuscula(*str);
         str++;
     }
 
 }
 
 int main() {
-    char texto[] = "iarley muito boiolA.";
+    //char texto[] = "Caio 2401371, Estevan 2400524, Iarley 2400918, Mikeias 2400176"; // 4 letras maiúsculas, também funciona para mais de uma palavra e com números
+    //char texto[] = "A Arte da Guerra.";
+    //char texto[] = "abc"; //ABC
+    //char texto[] = "Hello"; //HELLO
+    //char texto[] = "123abc"; //123ABC
+    //char texto[] = "teste"; //TESTE
+    //char texto[] = "ABC"; //ABC
+    //char texto[] = "aBcD"; //ABCD
+    //char texto[] = ""; //
+    //char texto[] = "ola mundo"; //OLA MUNDO
+    //char texto[] = "java"; //JAVA
+    char texto[] = "c123"; //C123
     converter_para_maiusculas(texto);
-    printf("Texto em maiúsculas: %s\n", texto);
+    printf("%s\n", texto);
     return 0;
 }
--- Exercicio-9/ConverterMinusculas.c
+++ Exercicio-9/ConverterMinusculas.c
@@ -1,17 +1,40 @@
+/**
+ * @brief Converte todos os caracteres da string para minúsculas
+ * @param str String a ser modificada (deve ser modificável)
+ */
+
+#include <ctype.h>
+#include <stdio.h>
 #include <string.h>
+
+char minuscula(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + 32;
+    }
+    return c;
+}
 
 void converter_para_minusculas(char *str) {
     while (*str) {
-        if (*str >= 'A' && *str <= 'Z') {
-            *str = *str + 32;
-        }
+        *str = minuscula(*str);
         str++;
     }
 }
 
 int main() {
-    char texto[] = "IARLEY MUITO.";
+    //char texto[] = "Caio 2401371, Estevan 2400524, Iarley 2400918, Mikeias 2400176"; // 4 letras maiúsculas, também funciona para mais de uma palavra e com números
+    //char texto[] = "Conhecimento Mútuo: Se você conhece o inimigo e a si mesmo, não precisa temer o resultado de cem batalhas";
+    //char texto[] = "ABC";
+    //char texto[] = "hELLO";
+    //char texto[] = "123ABC";
+    //char texto[] = "TESTE";
+    //char texto[] = "ABC";
+    //char texto[] = "AbCd";
+    //char texto[] = "";
+    //char texto[] = "OLA MUNDO";
+    //char texto[] = "JAVA";
+    char texto[] = "C123";
     converter_para_minusculas(texto);
-    printf("minúsculas: %s\n", texto);
+    printf("%s\n", texto);
     return 0;
 }
